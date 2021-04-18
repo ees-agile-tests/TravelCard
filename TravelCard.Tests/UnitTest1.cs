52d45d4c818dd98e79ce1e1a9143a22d7fa89e91
João Paulo de Farias <jpaulodefarias@gmail.com>
2021-04-18 19:02:47 -0300

    Initial commit

---
+++ TravelCard.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TravelCard.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TravelCard/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TravelCard
+{
+    public class Class1
+    {
+    }
+}
