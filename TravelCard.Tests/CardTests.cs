9493c62e54679759a424bc5726d0618315284351
João Paulo de Farias <jpaulodefarias@gmail.com>
2021-04-20 00:16:48 -0300

    Refactoring CardTests

--- TravelCard.Tests/CardTests.cs
+++ TravelCard.Tests/CardTests.cs
@@ -10,7 +10,6 @@ namespace TravelCard.Tests
         public void DebitAccountTest()
         {
             var bankAccountMock = new Mock<BankAccount>(It.IsAny<long>(), It.IsAny<decimal>());
-            bankAccountMock.Setup(x => x.DebitMoney(It.IsAny<decimal>()));
 
             var bankAccount = bankAccountMock.Object;
             var card = new Card(bankAccount);
